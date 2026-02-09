a5be9c07178293e65eb06761efd703e9f6515c9c
Abin Binoy <abinbinoy2000@gmail.com>
2026-02-09 20:01:00 +0530

    Initial commit - Abin Binoy Portfolio

---
+++ sources/data/social.js
@@ -0,0 +1,6 @@
+export default [
+    { name: 'Mail', url: 'mailto:abinbinoy2000@gmail.com', align: 'right' },
+    { name: 'GitHub', url: 'https://github.com/BROHUHA', align: 'right' },
+    { name: 'WhatsApp', url: 'https://wa.me/918075747414', align: 'right' },
+    { name: 'Instagram', url: 'https://instagram.com/abinn.binoyy', align: 'left' },
+]
